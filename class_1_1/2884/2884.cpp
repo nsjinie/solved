ff39a23d6549ce31e50c208195072d11710fe955
nsjinie <nsjinie@gmail.com>
2024-04-13 18:08:59 +0900

    2884

---
+++ class_1_1/2884/2884.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "rt", stdin);
+    int h, m;
+    cin >> h >> m;
+
+    m -= 45;
+    if (m < 0)
+    {
+        m += 60;
+        h -= 1;
+        if (h < 0)
+            h = 23;
+    }
+
+    cout << h << " " << m;
+
+    return 0;
+}
