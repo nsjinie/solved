2b8a87d169a68bfb829b9bbad4efde6c2f7b25e3
nsjinie <nsjinie@gmail.com>
2024-04-14 04:49:01 +0900

    10951

---
+++ class_1_2/10951/10951.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // freopen("input.txt", "rt", stdin);
+    int a, b;
+    while (cin >> a >> b)
+    {
+        cout << a + b << '\n';
+    }
+    return 0;
+}
