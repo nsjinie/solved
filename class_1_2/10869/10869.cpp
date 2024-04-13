e5241daf6eb3812afb99830c218b6c36f62901c8
nsjinie <nsjinie@gmail.com>
2024-04-13 19:41:40 +0900

    10869

---
+++ class_1_2/10869/10869.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // freopen("input.txt", "rt", stdin);
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+    cout << a - b << '\n';
+    cout << a * b << '\n';
+    cout << a / b << '\n';
+    cout << a % b << '\n';
+    return 0;
+}
