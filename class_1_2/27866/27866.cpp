fdf1dc04e3b1ce62e9752c02c7781b35d84e7875
nsjinie <nsjinie@gmail.com>
2024-04-14 15:50:26 +0900

    27866

---
+++ class_1_2/27866/27866.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // freopen("input.txt", "rt", stdin);
+    int n;
+    string str;
+    cin >> str >> n;
+    cout << str[n - 1];
+    return 0;
+}
