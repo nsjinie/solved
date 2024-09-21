4c65a0d98928396da73618e4c2891a138c1071cc
nsjinie <nsjinie@gmail.com>
2024-09-21 22:23:52 +0900

    c3 / S3 : 17626 Four Squares (DP)

---
+++ class_3/S3_17626_dp/s3_17626_dp.cpp
@@ -0,0 +1,36 @@
+// 문제 : 17626 Four Squares
+//       https://www.acmicpc.net/problem/17626
+// 시간 복잡도 : O(n2)
+// 알고리즘 : DP
+// 풀이 방향 : dp[i-j^2] 이 최소가 되는 값을 찾는다
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int dp[50001];
+
+int main()
+{
+    int n;
+    freopen("input.txt", "rt", stdin);
+    cin >> n;
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        int minn = INT_MAX;
+        for (int j = 1; j * j <= n; j++)
+        {
+            int tmp = i - (j * j);
+            minn = min(minn, dp[tmp]);
+        }
+        dp[i] = minn + 1;
+    }
+    cout << dp[n];
+
+    return 0;
+}
